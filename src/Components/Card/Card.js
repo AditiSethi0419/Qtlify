568173019c3bbaf9351ffa1eeec3224df6373f7f
Sagar Gupta <sagar.gupta@novoinvent.com>
2023-07-08 00:49:29 +0530

    card component

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div>
+        <div>
+            <img src={} alt="get my image"/>
+            <button>
+                <p>No of follows</p>
+            </button>
+        </div>
+        <h3>Type of song</h3>
+      
+    </div>
+  );
+}
+
+export default Card;
